1ed8167265586e328b627d12a23dfa93cdbdb1ec
Gustavo Kasper <kaspergustavo27@hotmail.com>
2024-10-14 20:00:21 -0300

    Ajustando a barra lateral e configurando a lista de tarefas

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,3 +1,21 @@
-const BarraLateral = () => <aside>barra lateral</aside>
+import FiltroCard from '../../components/FiltroCard'
+
+import * as S from './styles'
+
+const BarraLateral = () => (
+  <S.Aside>
+    <div>
+      <S.Campo type="text" placeholder="Buscar" />
+    </div>
+    <S.Filtros>
+      <FiltroCard legenda="Pendentes" contador={1} />
+      <FiltroCard legenda="Concluidas" contador={2} />
+      <FiltroCard legenda="Urgentes" contador={3} />
+      <FiltroCard legenda="Importantes" contador={4} />
+      <FiltroCard legenda="Normal" contador={5} />
+      <FiltroCard legenda="Todas" contador={10} />
+    </S.Filtros>
+  </S.Aside>
+)
 
 export default BarraLateral
