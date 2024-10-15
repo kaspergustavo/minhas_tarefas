36e29666ce79ede812623ee9174c8b233d564ea0
Gustavo Kasper <kaspergustavo27@hotmail.com>
2024-10-15 20:19:09 -0300

    Estilizando as tags com ENUMS

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -9,7 +9,7 @@ const BarraLateral = () => (
     </div>
     <S.Filtros>
       <FiltroCard legenda="Pendentes" contador={1} />
-      <FiltroCard legenda="Concluidas" contador={2} />
+      <FiltroCard legenda="Concluídas" contador={2} />
       <FiltroCard legenda="Urgentes" contador={3} />
       <FiltroCard legenda="Importantes" contador={4} />
       <FiltroCard legenda="Normal" contador={5} />
--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,25 +1,43 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
+import * as enums from '../../utils/enums/Tarefa'
+
+const tarefas = [
+  {
+    titulo: 'Estudar Typescript',
+    descricao: 'Ver aula 3 da EBAC',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  },
+  {
+    titulo: 'Pagar a conta de internet',
+    descricao: 'Baixar boleto no e-mail',
+    prioridade: enums.Prioridade.URGENTE,
+    status: enums.Status.CONCLUIDA
+  },
+  {
+    titulo: 'Ir para a academia',
+    descricao: 'Treino de pernas',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
